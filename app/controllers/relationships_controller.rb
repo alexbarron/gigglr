bd80637d25d04a7f1f9dc1819af86aa7df456f84
Alex Barron <alexbarron@gmail.com>
2015-11-22 14:55:06 -0800

    Follow and unfollow comedians from comedian index

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,13 +4,20 @@ class RelationshipsController < ApplicationController
 	def create
 		@comedian = Comedian.find(params[:comedian_id])
 		current_user.follow(@comedian)
-		redirect_to @comedian, notice: "You're now following #{@comedian.name}"
+		respond_to do |format|
+			format.html { redirect_to request.referer || @comedian, notice: "You're now following #{@comedian.name}" }
+			format.js
+		end
+
 	end
 
 	def destroy
 		@comedian = Relationship.find(params[:id]).comedian
 		current_user.unfollow(@comedian)
-		redirect_to @comedian, notice: "You're no longer following #{@comedian.name}"
+		respond_to do |format|
+			format.html { redirect_to request.referer || @comedian, notice: "You're no longer following #{@comedian.name}" }
+			format.js
+		end
 	end
 
 end
